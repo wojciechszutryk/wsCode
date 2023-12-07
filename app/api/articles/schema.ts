4fdae77ab67f6f10bb8d7f8a3c298b75f0123846
wojtekszutryk <wojtekszutryk@gmail.com>
2023-12-07 20:01:45 +0100

    add error handling

--- app/api/articles/schema.ts
+++ app/api/articles/schema.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
 export const createArticleSchema = z.object({
-    title: z.string().min(5).max(255),
-    description: z.string().min(5).max(255).optional(),
-    body: z.string().min(5).optional(),
-    authorId: z.number().int().positive(),
+  title: z
+    .string()
+    .min(5, "Title must be at least 5 characters long")
+    .max(255, `Title must be at most 255 characters long`),
+  description: z
+    .string()
+    .min(5, "Description must be at least 5 characters long")
+    .max(255, ` Description must be at most 255 characters long`)
+    .optional(),
+  body: z.string().min(5, "Body must be at least 5 characters long").optional(),
+  authorId: z.number().int().positive(),
 });
--- app/articles/new/page.tsx
+++ app/articles/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
@@ -8,6 +8,9 @@ import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { CreateArticleDto } from "@/models/articles/article.dto";
+import { MdError } from "react-icons/md";
+import axios, { Axios, AxiosError } from "axios";
+import Error from "next/error";
 
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
@@ -19,47 +22,66 @@ const NewArticlePage = () => {
       authorId: 1,
     },
   });
+  const [error, setError] = useState<string | null>(null);
+
+  const handleCreateArticle = async (data: CreateArticleDto) => {
+    try {
+      await axios.post("/api/articles", data);
+      router.push("/articles");
+    } catch (error) {
+      console.log("error: ", error);
+
+      if (error instanceof AxiosError) {
+        console.log("error.response: ", error.response);
+
+        setError(error?.response?.data?.[0]?.message);
+      } else {
+        setError("Something went wrong, please try again later");
+      }
+    }
+  };
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        console.log("data", data);
-
-        await fetch("/api/articles", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
-        });
-        router.push("/articles");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input
-          placeholder="new issue"
-          {...register("title", {
-            required: true,
-            minLength: 5,
-            maxLength: 255,
-          })}
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <MdError />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(handleCreateArticle)}
+      >
+        <TextField.Root>
+          <TextField.Input
+            placeholder="new issue"
+            {...register("title", {
+              required: true,
+              minLength: 5,
+              maxLength: 255,
+            })}
+          />
+        </TextField.Root>
+        <TextField.Root>
+          <TextField.Input
+            placeholder="description"
+            {...register("description", {
+              required: false,
+              minLength: 5,
+            })}
+          />
+        </TextField.Root>
+        <Controller
+          name={"body"}
+          control={control}
+          render={({ field }) => <MDEditor {...field} />}
         />
-      </TextField.Root>
-      <TextField.Root>
-        <TextField.Input
-          placeholder="description"
-          {...register("description", {
-            required: false,
-            minLength: 5,
-          })}
-        />
-      </TextField.Root>
-      <Controller
-        name={"body"}
-        control={control}
-        render={({ field }) => <MDEditor {...field} />}
-      />
-      <Button className="hover:cursor-pointer">Create article</Button>
-    </form>
+        <Button className="hover:cursor-pointer">Create article</Button>
+      </form>
+    </div>
   );
 };
 
